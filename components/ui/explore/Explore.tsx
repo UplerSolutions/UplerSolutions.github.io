3f8528901eb60ea66f5d715160aa203c8962ed98
Serafin Quesada <serafinkocho@gmail.com>
2023-08-17 15:59:58 -0300

    pagination complete

--- components/ui/explore/Explore.tsx
+++ components/ui/explore/Explore.tsx
@@ -1,168 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Pagination from '@mui/material/Pagination'
 import Link from 'next/link'
 import Image from "next/image"
-export const Explore = () => {
-  return (
-    <div className='flex flex-col xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md w-full m-auto px-4 py-4 lg:py-8 items-center'>
-      <ul className='grid grid-cols-[repeat(auto-fill,_minmax(310px,_1fr))] gap-4'>
-        <li className=''>
-          <Link href=''>
-            <div className='border flex flex-col h-full bg-white text-neutral-950 rounded-xl'>
-              <div className='relative p-6 w-full h-full bg-primary-color rounded-xl'>
-                <Image height={200} width={200} src='/isotipo.png' alt='logo' />
-              </div>
-              <div className='p-4 flex flex-col flex-1'>
-                <div>
-                  <h2 className='text-2xl'>Proyecto</h2>
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Necessitatibus quam asperiores assumenda eius odit, tempora
-                    nostrum quaerat laborum.
-                  </p>
-                  <p className='text-neutral-500 py-3'>equipo del proyecto</p>
-                </div>
+import { data } from '@/pages/api/marketplace';
 
-                <div>
-                  <hr className=' my-1 mx-1 h-2 bg-primary-color rounded border-0' />
-                  <p className='mx-1 text-primary-color font-semibold'>
-                    70% financiado
-                  </p>
-                </div>
-              </div>
-            </div>
-          </Link>
-        </li>
-        <li className=''>
-          <Link href=''>
-            <div className='border flex flex-col h-full bg-white text-neutral-950 rounded-xl'>
-              <div className='relative p-6 w-full h-full bg-primary-color rounded-xl'>
-                <Image height={200} width={200} src='/isotipo.png' alt='logo' />
-              </div>
-              <div className='p-4 flex flex-col flex-1'>
-                <div>
-                  <h2 className='text-2xl'>Proyecto</h2>
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Necessitatibus quam asperiores assumenda eius odit, tempora
-                    nostrum quaerat laborum.
-                  </p>
-                  <p className='text-neutral-500 py-3'>equipo del proyecto</p>
-                </div>
-              </div>
-            </div>
-          </Link>
-        </li> 
-        <li className=''>
-          <Link href=''>
-            <div className='border flex flex-col h-full bg-white text-neutral-950 rounded-xl'>
-              <div className='relative p-6 w-full h-full bg-primary-color rounded-xl'>
-                <Image height={200} width={200} src='/isotipo.png' alt='logo' />
-              </div>
-              <div className='p-4 flex flex-col flex-1'>
-                <div>
-                  <h2 className='text-2xl'>Proyecto</h2>
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Necessitatibus quam asperiores assumenda eius odit, tempora
-                    nostrum quaerat laborum.
-                  </p>
-                  <p className='text-neutral-500 py-3'>equipo del proyecto</p>
-                </div>
-
-                <div>
-                  <hr className=' my-1 mx-1 h-2 bg-primary-color rounded border-0' />
-                  <p className='mx-1 text-primary-color font-semibold'>
-                    70% financiado
-                  </p>
-                </div>
-              </div>
-            </div>
-          </Link>
-        </li>
-        <li className=''>
-          <Link href=''>
-            <div className='border flex flex-col h-full bg-white text-neutral-950 rounded-xl'>
-              <div className='relative p-6 w-full h-full bg-primary-color rounded-xl'>
-                <Image height={200} width={200} src='/isotipo.png' alt='logo' />
-              </div>
-              <div className='p-4 flex flex-col flex-1'>
-                <div>
-                  <h2 className='text-2xl'>Proyecto</h2>
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Necessitatibus quam asperiores assumenda eius odit, tempora
-                    nostrum quaerat laborum.
-                  </p>
-                  <p className='text-neutral-500 py-3'>equipo del proyecto</p>
-                </div>
-
-                <div>
-                  <hr className=' my-1 mx-1 h-2 bg-primary-color rounded border-0' />
-                  <p className='mx-1 text-primary-color font-semibold'>
-                    70% financiado
-                  </p>
-                </div>
-              </div>
-            </div>
-          </Link>
-        </li>
-        <li className=''>
-          <Link href=''>
-            <div className='border flex flex-col h-full bg-white text-neutral-950 rounded-xl'>
-              <div className='relative p-6 w-full h-full bg-primary-color rounded-xl'>
-                <Image height={200} width={200} src='/isotipo.png' alt='logo' />
-              </div>
-              <div className='p-4 flex flex-col flex-1'>
-                <div>
-                  <h2 className='text-2xl'>Proyecto</h2>
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Necessitatibus quam asperiores assumenda eius odit, tempora
-                    nostrum quaerat laborum.
-                  </p>
-                  <p className='text-neutral-500 py-3'>equipo del proyecto</p>
-                </div>
-
-                <div>
-                  <hr className=' my-1 mx-1 h-2 bg-primary-color rounded border-0' />
-                  <p className='mx-1 text-primary-color font-semibold'>
-                    70% financiado
-                  </p>
-                </div>
-              </div>
-            </div>
-          </Link>
-        </li>
-        <li className=''>
-          <Link href=''>
-            <div className='border flex flex-col h-full bg-white text-neutral-950 rounded-xl'>
-              <div className='relative p-6 w-full h-full bg-primary-color rounded-xl'>
-                <Image height={200} src='/isotipo.png' alt='logo' width={200} />
-              </div>
-              <div className='p-4 flex flex-col flex-1'>
-                <div>
-                  <h2 className='text-2xl'>Proyecto</h2>
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Necessitatibus quam asperiores assumenda eius odit, tempora
-                    nostrum quaerat laborum.
-                  </p>
-                  <p className='text-neutral-500 py-3'>equipo del proyecto</p>
-                </div>
-
-                <div>
-                  <hr className=' my-1 mx-1 h-2 bg-primary-color rounded border-0' />
-                  <p className='mx-1 text-primary-color font-semibold'>
-                    70% financiado
-                  </p>
-                </div>
-              </div>
-            </div>
-          </Link>
-        </li>
-      </ul>
-      <Pagination count={10} className='pt-10' />
-    </div>
-  )
+interface Product {
+  ID: number;
+  title: string;
+  price: number;
+  description: string;
+  // Add other properties as needed
 }
+
+export const Explore = () => {
+  const pageSize = 6;
+  const totalProducts = data.length;
+  const totalPages = Math.ceil(totalProducts / pageSize);
+
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const onPageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+  const handlePaginationChange = (event: React.ChangeEvent<unknown>, page: number) => {
+    onPageChange(page);
+  };
+
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const displayedProducts: Product[] = data.slice(startIndex, endIndex);
+
+  return (
+    <div className='grid-cols-[repeat(auto-fill,_minmax(350,_1fr))] grid-rows-2 gap-4 xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md w-full m-auto px-4 py-4 lg:py-8 items-center'>
+      <ul className=''>
+        {displayedProducts.map(product => (
+          <li key={product.ID} className=''>
+            <div className='border flex flex-col h-full bg-white text-neutral-950 rounded-xl'>
+              <div className='relative p-6 w-full h-full bg-primary-color rounded-xl'>
+                {/* Render product image here */}
+              </div>
+              <div className='p-4 flex flex-col flex-1'>
+                <div>
+                  <h2 className='text-2xl'>{product.title}</h2>
+                  <p>${product.price} price </p>
+                  <p>{product.description}</p>
+                </div>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <Pagination
+        count={totalPages }
+        page={currentPage}
+        onChange={handlePaginationChange}
+        className='pt-10'
+      />
+    </div>
+  );
+};
--- pages/api/marketplace.js
+++ pages/api/marketplace.js
@@ -1,110 +1,129 @@
-export const data = [
+export const data = 
+[
     {
         "ID": 1,
         "title": "SmartTask Manager",
         "description": "A sophisticated task management software that helps you organize your tasks, set priorities, and collaborate with your team.",
-        "image": "https://example.com/images/smarttask_manager.svg"
+        "image": "https://example.com/images/smarttask_manager.svg",
+        "price": 16
     },
     {
         "ID": 2,
         "title": "SecureVault",
         "description": "An advanced encryption software that allows you to securely store and manage your sensitive documents and files.",
-        "image": "https://example.com/images/securevault.svg"
+        "image": "https://example.com/images/securevault.svg",
+        "price": 10
     },
     {
         "ID": 3,
         "title": "HealthTrack Pro",
         "description": "A comprehensive health and fitness tracking application that monitors your exercise, nutrition, and overall wellness.",
-        "image": "https://example.com/images/healthtrack_pro.svg"
+        "image": "https://example.com/images/healthtrack_pro.svg",
+        "price": 21
     },
     {
         "ID": 4,
         "title": "AIWriter Studio",
         "description": "An AI-powered writing assistant that helps you generate high-quality content, provides suggestions, and checks for grammar and style errors.",
-        "image": "https://example.com/images/aiwriter_studio.svg"
+        "image": "https://example.com/images/aiwriter_studio.svg",
+        "price": 8
     },
     {
         "ID": 5,
         "title": "CodeGenius IDE",
         "description": "An integrated development environment (IDE) designed to boost productivity for programmers with advanced code analysis and debugging tools.",
-        "image": "https://example.com/images/codegenius_ide.svg"
+        "image": "https://example.com/images/codegenius_ide.svg",
+        "price": 14
     },
     {
         "ID": 6,
         "title": "FinanceMaster",
         "description": "A comprehensive financial management software that helps you track expenses, create budgets, and analyze investment opportunities.",
-        "image": "https://example.com/images/financemaster.svg"
+        "image": "https://example.com/images/financemaster.svg",
+        "price": 19
     },
     {
         "ID": 7,
         "title": "DesignFusion",
         "description": "A creative design software with a wide range of tools for graphic designers to create stunning visuals and illustrations.",
-        "image": "https://example.com/images/designfusion.svg"
+        "image": "https://example.com/images/designfusion.svg",
+        "price": 12
     },
     {
         "ID": 8,
         "title": "LanguageLearn Pro",
         "description": "An immersive language learning platform that uses AI to personalize lessons and accelerate your language acquisition.",
-        "image": "https://example.com/images/languagelearn_pro.svg"
+        "image": "https://example.com/images/languagelearn_pro.svg",
+        "price": 24
     },
     {
         "ID": 9,
         "title": "DataShield Suite",
         "description": "An all-in-one data protection suite that offers encryption, secure data backup, and real-time threat monitoring.",
-        "image": "https://example.com/images/datashield_suite.svg"
+        "image": "https://example.com/images/datashield_suite.svg",
+        "price": 6
     },
     {
         "ID": 10,
         "title": "VirtualSculptor",
         "description": "A virtual sculpting software that enables artists to create intricate 3D sculptures using intuitive digital tools.",
-        "image": "https://example.com/images/virtualsculptor.svg"
+        "image": "https://example.com/images/virtualsculptor.svg",
+        "price": 22
     },
     {
         "ID": 11,
         "title": "SmartHome Control",
         "description": "A centralized smart home control application that lets you manage and automate various IoT devices in your home.",
-        "image": "https://example.com/images/smarthome_control.svg"
+        "image": "https://example.com/images/smarthome_control.svg",
+        "price": 7
     },
     {
         "ID": 12,
         "title": "MusicMixer Pro",
         "description": "An advanced music mixing and production software with a wide range of effects and tools for professional musicians and producers.",
-        "image": "https://example.com/images/musicmixer_pro.svg"
+        "image": "https://example.com/images/musicmixer_pro.svg",
+        "price": 18
     },
     {
         "ID": 13,
         "title": "EduConnect LMS",
         "description": "An innovative Learning Management System (LMS) designed to facilitate online education and training with interactive features.",
-        "image": "https://example.com/images/educonnect_lms.svg"
+        "image": "https://example.com/images/educonnect_lms.svg",
+        "price": 11
     },
     {
         "ID": 14,
         "title": "RetailOptima",
         "description": "A data-driven retail management software that optimizes inventory, analyzes sales trends, and enhances customer experience.",
-        "image": "https://example.com/images/retailoptima.svg"
+        "image": "https://example.com/images/retailoptima.svg",
+        "price": 15
     },
     {
         "ID": 15,
         "title": "TravelPlanner Plus",
         "description": "A comprehensive travel planning application that helps users organize itineraries, book accommodations, and discover attractions.",
-        "image": "https://example.com/images/travelplanner_plus.svg"
+        "image": "https://example.com/images/travelplanner_plus.svg",
+        "price": 23
     },
     {
         "ID": 16,
         "title": "VRGameMaker Studio",
         "description": "A virtual reality game development platform that empowers game developers to create immersive VR experiences without extensive coding.",
-        "image": "https://example.com/images/vrgamemaker_studio.svg"
+        "image": "https://example.com/images/vrgamemaker_studio.svg",
+        "price": 13
     },
     {
         "ID": 17,
         "title": "MindSync Meditation",
         "description": "A meditation and mindfulness app that offers guided sessions, ambient sounds, and brainwave entrainment for stress relief.",
-        "image": "https://example.com/images/mindsync_meditation.svg"
+        "image": "https://example.com/images/mindsync_meditation.svg",
+        "price": 9
     },
     {
         "ID": 18,
         "title": "AIChef Companion",
         "description": "An AI-driven cooking assistant that provides recipes, cooking tips, and personalized meal suggestions based on dietary preferences.",
-        "image": "https://example.com/images/aichef_companion.svg"
+        "image": "https://example.com/images/aichef_companion.svg",
+        "price": 20
     }
 ]
