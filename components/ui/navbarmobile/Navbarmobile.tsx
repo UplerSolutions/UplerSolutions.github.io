8feae8acce7dd18d03b3551d0586ee8f39794a39
JoaquinMarmol <joaquinmarmolnecochea@gmail.com>
2023-08-22 19:34:34 -0300

    responsive bugs

--- components/ui/navbarmobile/Navbarmobile.tsx
+++ components/ui/navbarmobile/Navbarmobile.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
-import { PiRocket,PiClipboard } from 'react-icons/pi'
+import { PiRocket, PiClipboard } from 'react-icons/pi'
 import { BiPackage } from 'react-icons/bi'
 import { AiOutlineHome } from 'react-icons/ai'
 
 export const Navbarmobile = () => {
   return (
-    <nav className='flex lg:hidden items-center justify-center w-full h-[40px]'>
-      <div className='flex z-10 w-full text-lg items-center justify-center font-mono lg:hidden bg-neutral-100 text-neutral-700 pt-3 pb-3 gap-3 fixed bottom-0 h-[50px]'>
+    <nav className='flex lg:hidden items-center justify-center w-full'>
+      <div className='flex z-10 w-full text-lg items-center justify-center font-mono lg:hidden bg-neutral-100 text-neutral-700 pt-3 pb-3 gap-3 fixed bottom-0 h-[45px]'>
         <Link className='flex flex-col w-1/6 text-center items-center' href='/'>
           <AiOutlineHome />
           <span className=' text-xs'>Home</span>
--- pages/software/index.tsx
+++ pages/software/index.tsx
@@ -17,32 +17,32 @@ const Startups: NextPage = () => {
   return (
     <Layout title='startups'>
       <section className='flex flex-col justify-center items-center pt-10 pb-20 bg-gradient-to-r from-[#fde9fc] to-[#fffbe0]'>
-      <div className='flex flex-col gap-8 pb-4 items-center text-center'>
-              <h1 className='text-[32px] text-center font-bold md:text-5xl lg:text-6xl text-neutral-700'>Softwares</h1>
-      <p className='text-xl'>Explore our collection and find your solution.</p>
-      </div>
-      <div className='flex gap-5 xl:gap-[240px] py-8'>
-        <button className=' bg-primary-color h-12 w-48 rounded-xl text-white font-semibold hover:bg-fuchsia-200 hover:text-primary-color transition hover:delay-100 hover:border-2 hover:border-primary-color'>
-        Established Software
-        </button>
-        <button
-            className='group relative inline-block overflow-hidden rounded-xl border border-fuchsia-100 bg-fuchsia-200 px-2 lg:px-6 py-3 text-sm font-semibold text-primary-color hover:text-primary-color focus:outline-none focus:ring active:bg-primary-color active:text-white'
-          >
-              <span className='ease absolute left-0 top-0 h-0 w-0 border-t-2 border-primary-color transition-all duration-500 group-hover:w-full'></span>
-              <span className='ease absolute right-0 top-0 h-0 w-0 border-r-2 border-primary-color transition-all duration-500 group-hover:h-full'></span>
-              <span className='ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-primary-color transition-all duration-500 group-hover:w-full'></span>
-              <span className='ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-primary-color transition-all duration-500 group-hover:h-full'></span>
+        <div className='flex flex-col gap-8 pb-4 items-center text-center'>
+          <h1 className='text-[32px] text-center font-bold md:text-5xl lg:text-6xl text-neutral-700'>
+            Softwares
+          </h1>
+          <p className='text-xl text-neutral-700'>
+            Explore our collection and find your solution.
+          </p>
+        </div>
+        <div className='flex gap-5 xl:gap-[240px] py-8'>
+          <button className=' bg-primary-color h-12 w-48 rounded-xl text-white font-semibold hover:bg-fuchsia-200 hover:text-primary-color transition hover:delay-100 hover:border-2 hover:border-primary-color'>
+            Established Software
+          </button>
+          <button className='group relative inline-block overflow-hidden rounded-xl border border-fuchsia-100 bg-fuchsia-200 px-2 lg:px-6 py-3 text-sm font-semibold text-primary-color hover:text-primary-color focus:outline-none focus:ring active:bg-primary-color active:text-white'>
+            <span className='ease absolute left-0 top-0 h-0 w-0 border-t-2 border-primary-color transition-all duration-500 group-hover:w-full'></span>
+            <span className='ease absolute right-0 top-0 h-0 w-0 border-r-2 border-primary-color transition-all duration-500 group-hover:h-full'></span>
+            <span className='ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-primary-color transition-all duration-500 group-hover:w-full'></span>
+            <span className='ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-primary-color transition-all duration-500 group-hover:h-full'></span>
             Startups Softwares
-        </button>
-      
-      </div>
+          </button>
+        </div>
 
         <div className=' flex justify-center w-full'>
-          <Box maxWidth={'sm'} margin='auto' className =" xl:w-[100%]">
+          <Box maxWidth={'sm'} margin='auto' className=' xl:w-[100%]'>
             <Typography textAlign='center' my={2}></Typography>
             <Box ref={anchorEl}>
               <SearchBar
-             
                 onSubmit={(searchTerm: string) => {
                   // when the user submits the form, we only modify the router query parameters
                   router.push({
