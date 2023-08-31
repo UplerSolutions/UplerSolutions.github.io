075965f8cb79e5d406c954bc2121aab59a28c47f
JoaquinMarmol <joaquinmarmolnecochea@gmail.com>
2023-08-31 10:05:51 -0300

    footer

--- components/ui/contributions/Contributions.tsx
+++ components/ui/contributions/Contributions.tsx
@@ -1,55 +1,51 @@
-import React, { useEffect, useState, FC } from 'react';
+import React, { useEffect, useState, FC } from 'react'
 //styling
 
 interface Count {
-  id: string;
-  label: string;
-  number: string;
-  duration: string;
+  id: string
+  label: string
+  number: string
+  duration: string
 }
 interface Props {
   count: Count
-
 }
-export const Contributions :FC<Props> =({count }) => {
+export const Contributions: FC<Props> = ({ count }) => {
   // label of counter
   // number to increment to
   // duration of count in secondscounts
-  const {label, number, duration} = count;
+  const { label, number, duration } = count
 
   // number displayed by component
-  const [counts, setCounts] = useState("0")
+  const [counts, setCounts] = useState('0')
 
   useEffect(() => {
-    let start = 0;
+    let start = 0
     // first three numbers from props
-    const end = parseInt(number?.substring(0,3))
+    const end = parseInt(number?.substring(0, 3))
     // if zero, return
-    if (start === end) return;
+    if (start === end) return
 
     // find duration per increment
-    let totalMilSecDur = parseInt(duration);
-    let incrementTime = (totalMilSecDur / end) * 1000;
+    let totalMilSecDur = parseInt(duration)
+    let incrementTime = (totalMilSecDur / end) * 1000
 
-    // timer increments start countser 
+    // timer increments start countser
     // then updates counts
     // ends if start reaches end
     let timer = setInterval(() => {
-      start += 1;
+      start += 1
       setCounts(String(start) + number.substring(3))
-      if (start === end) clearInterval(timer)       
-    }, incrementTime);
+      if (start === end) clearInterval(timer)
+    }, incrementTime)
 
     // dependency array
-  }, [number, duration]);
+  }, [number, duration])
 
-  return (   
+  return (
     <div className='flex flex-col h-20 text-center md:w-full px-2'>
-      <span className=' text-2xl md:text-4xl text-primary-color'>{counts}
-      </span>
-      <span className=' text-md md:text-xl'>{label}
-      </span>
+      <span className=' text-2xl md:text-4xl text-primary-color'>{counts}</span>
+      <span className=' text-md md:text-xl'>{label}</span>
     </div>
-  );
+  )
 }
-
