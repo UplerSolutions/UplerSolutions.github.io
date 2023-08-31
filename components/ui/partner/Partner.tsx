e68d35d4917abcf9d137160c74e4182f5306446a
JoaquinMarmol <joaquinmarmolnecochea@gmail.com>
2023-08-31 09:19:28 -0300

    sponsors

--- components/ui/partner/Partner.tsx
+++ components/ui/partner/Partner.tsx
@@ -1,16 +1,16 @@
-import Image from "next/image"
+import Image from 'next/image'
 
 export const Partner = () => {
   return (
-    <section className='flex items-center justify-center flex-col pb-6 pt-8  text-neutral-600 font-semibold '>
+    <section className='flex items-center  md:items-stretch  justify-center flex-col pb-6 pt-8  text-neutral-600 font-semibold '>
       <div className='flex flex-col text-left lg:w-[100%]'>
         <div className='text-center flex items-center lg:text-left pb-10 w-full'>
           <div className='flex flex-col'>
             <h3 className='pb-4 text-xs md:text-base lg:text-lg'>
-            GET TO KNOW WHO TRUSTS US
+              GET TO KNOW WHO TRUSTS US
             </h3>
             <h2 className='text-2xl lg:text-4xl'>
-            Our Official
+              Our Official
               <span className='text-primary-color'> Partnerships</span>
             </h2>
           </div>
@@ -18,54 +18,45 @@ export const Partner = () => {
             <h3 className=''>CONOCE QUIEN CONFIA EN NOSOTROS</h3>
             <h2 className='text-4xl'>
               Nuestros
-              <span className='text-primary-color0'> partnerships oficiales</span>
+              <span className='text-primary-color0'>
+                {' '}
+                partnerships oficiales
+              </span>
             </h2>
           </div>
         </div>
       </div>
 
-      <div className='flex flex-wrap justify-center sm:flex-row '>
+      <div className='flex flex-wrap justify-center lg:justify-between sm:flex-row gap-8 md:gap-8'>
         <Image
           height={120}
-          width={120}
-          src='/isotipo.png'
+          width={320}
+          src='/Bybit.png'
           alt='partner-logo'
-          className='p-8 flew-wrap w-28 md:w-[180px] md:flex-wrap lg:w-[230px]'
+          className='md:py-24 flew-wrap w-[40%] md:w-[180px] md:flex-wrap 2xl:w-[330px]'
         />
         <Image
-          height={120}
-          width={120}
-          src='/isotipo.png'
+          height={80}
+          width={400}
+          src='/prex.png'
           alt='partner-logo'
-          className='p-8 flew-wrap w-28 md:w-[180px] md:flex-wrap lg:w-[230px]'
+          className='md:py-24 flew-wrap w-[40%] md:w-[180px] md:flex-wrap 2xl:w-[300px] '
         />
+
+        <Image
+          height={50}
+          width={190}
+          src='/damke.png'
+          alt='partner-logo'
+          className='md:py-8 md:mx-8 flew-wrap w-[40%] md:w-[180px] md:flex-wrap 2xl:w-[230px]'
+        />
+
         <Image
           height={120}
-          width={120}
-          src='/isotipo.png'
+          width={220}
+          src='/vincapital.png'
           alt='partner-logo'
-          className='p-8 flew-wrap w-28 md:w-[180px] md:flex-wrap lg:w-[230px]'
-        />
-        <Image
-          height={120}
-          width={120}
-          src='/isotipo.png'
-          alt='partner-logo'
-          className='p-8 flew-wrap w-28 md:w-[180px] md:flex-wrap lg:w-[230px]'
-        />
-        <Image
-          height={120}
-          width={120}
-          src='/isotipo.png'
-          alt='partner-logo'
-          className='p-8 flew-wrap w-28 md:w-[180px] md:flex-wrap lg:w-[230px]'
-        />
-        <Image
-          height={120}
-          width={120}
-          src='/isotipo.png'
-          alt='partner-logo'
-          className='p-8 flew-wrap w-28 md:w-[180px] md:flex-wrap lg:w-[230px]'
+          className='lg:md-8 flew-wrap w-[40%] md:w-[180px] md:flex-wrap 2xl:w-[230px]'
         />
       </div>
     </section>
