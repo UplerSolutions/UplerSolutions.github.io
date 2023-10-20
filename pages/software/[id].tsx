ad7ad0e67e0db9c819356d9aad36708bf2af28cf
JoaquinMarmol <joaquinmarmolnecochea@gmail.com>
2023-10-20 12:54:21 -0300

    marketplace with get link

--- pages/software/[id].tsx
+++ pages/software/[id].tsx
@@ -36,7 +36,7 @@ const Software: NextPage<Props> = ({ software }) => {
     setIsOpen(!isOpen)
   }
   return (
-    <div>
+    /*<div>
       {isOpen && (
         <div>
           <div className='flex flex-col justify-between'>
@@ -93,77 +93,47 @@ const Software: NextPage<Props> = ({ software }) => {
               </div>
             </nav>
           </div>
-          <div className='opacity-40'>
-            <Layout title='Upler - Software'>
-              <div className='flex flex-col md:flex-row items-center md:items-start  justify-center pt-28  bg-gradient-to-r from-[#fde9fc] to-[#fffbe0] h-[90vh] md:gap-8 pb-10'>
-                <div className='border w-[90%] md:w-[30%] flex flex-col  bg-white text-neutral-950 rounded-xl mt-9'>
-                  <div className='relative p-6 w-full h-[30vh] md:h-[50vh] bg-primary-color rounded-xl '></div>
-                </div>
-                <div className='w-full md:w-[30%] h-[70vh] p-6 rounded-md'>
-                  <div className='flex flex-col flex-1 gap-1'>
-                    <h2 className='text-xl md:text-2xl lg:text-3xl text-neutral-600 py-3 font-semibold'>
-                      {software.productName}
-                    </h2>
-                    <p className='text-primary-color text-xl md:text-2xl lg:text-3xl font-semibold flex gap-6 items-center'>
-                      ${software.price}
-                      <button
-                        onClick={handleClick}
-                        type='submit'
-                        className=' text-lg bg-primary-color h-12 w-48 rounded-xl text-white font-semibold hover:bg-fuchsia-200 hover:text-primary-color transition hover:delay-100 hover:border-2 hover:border-primary-color'
-                      >
-                        Buy now
-                      </button>
-                    </p>
-                  </div>
-
-                  <h1 className='text-xl md:text-2xl lg:text-3xl text-neutral-600 py-3 font-semibold'>
-                    Description
-                  </h1>
-                  <p className='text-neutral-500'>{software.longDescription}</p>
-                </div>
-              </div>
-            </Layout>
-          </div>
         </div>
       )}
       <div
         className={` ${
-          isOpen ? 'hidden' : '' // we can add styles when is open
+          isOpen ? 'opacity-30' : '' // we can add styles when is open
         }`}
-      >
-        <Layout title='Upler - Software'>
-          <div className='flex flex-col md:flex-row items-center md:items-start  justify-center pt-28  bg-gradient-to-r from-[#fde9fc] to-[#fffbe0] h-[90vh] md:gap-8 pb-10'>
-            <div className='border w-[90%] md:w-[30%] flex flex-col  bg-white text-neutral-950 rounded-xl mt-9'>
-              <div className='relative p-6 w-full bg-primary-color rounded-xl md:h-[58vh]'>
-                <img src={software.imageUrl} alt='' className='w-full' />
-              </div>
-            </div>
-            <div className='w-full md:w-[30%] h-[70vh] p-6 rounded-md'>
-              <div className='flex flex-col flex-1 gap-1'>
-                <h2 className='text-xl md:text-2xl lg:text-3xl text-neutral-600 py-3 font-semibold'>
-                  {software.productName}
-                </h2>
-                <p className='text-primary-color text-xl md:text-2xl lg:text-3xl font-semibold flex gap-6 items-center'>
-                  ${software.price}
-                  <button
-                    onClick={handleClick}
-                    type='submit'
-                    className=' text-lg bg-primary-color h-12 w-48 rounded-xl text-white font-semibold hover:bg-fuchsia-200 hover:text-primary-color transition hover:delay-100 hover:border-2 hover:border-primary-color'
-                  >
-                    Buy now
-                  </button>
-                </p>
-              </div>
-
-              <h1 className='text-xl md:text-2xl lg:text-3xl text-neutral-600 py-3 font-semibold'>
-                Description
-              </h1>
-              <p className='text-neutral-500'>{software.longDescription}</p>
-            </div>
+      >*/
+    <Layout title='Upler - Software'>
+      <div className='flex flex-col md:flex-row items-center md:items-start  justify-center pt-28  bg-gradient-to-r from-[#fde9fc] to-[#fffbe0] h-[90vh] md:gap-8 pb-10'>
+        <div className='border w-[90%] md:w-[30%] flex flex-col  bg-white text-neutral-950 rounded-xl mt-9'>
+          <div className='relative p-6 w-full bg-primary-color rounded-xl md:h-[58vh]'>
+            <img src={software.imageUrl} alt='' className='w-full' />
           </div>
-        </Layout>
+        </div>
+        <div className='w-full md:w-[30%] h-[70vh] p-6 rounded-md'>
+          <div className='flex flex-col flex-1 gap-1'>
+            <h2 className='text-xl md:text-2xl lg:text-3xl text-neutral-600 py-3 font-semibold'>
+              {software.productName}
+            </h2>
+            <p className='text-primary-color text-xl md:text-2xl lg:text-3xl font-semibold flex gap-6 items-center'>
+              ${software.price}
+              <Link href={software.directLink}>
+                <button
+                  type='submit'
+                  className=' text-lg bg-primary-color h-12 w-48 rounded-xl text-white font-semibold hover:bg-fuchsia-200 hover:text-primary-color transition hover:delay-100 hover:border-2 hover:border-primary-color'
+                >
+                  Get Link
+                </button>
+              </Link>
+            </p>
+          </div>
+
+          <h1 className='text-xl md:text-2xl lg:text-3xl text-neutral-600 py-3 font-semibold'>
+            Description
+          </h1>
+          <p className='text-neutral-500'>{software.longDescription}</p>
+        </div>
       </div>
-    </div>
+    </Layout>
+    /* </div>
+    </div>*/
   )
 }
 
