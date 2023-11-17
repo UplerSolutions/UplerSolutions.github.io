5cd4681d793938bb95d1fdb24965a68e16e66241
JoaquinMarmol <joaquinmarmolnecochea@gmail.com>
2023-11-17 20:13:49 -0300

    team data

---
+++ data/team.ts
@@ -0,0 +1,45 @@
+export const team = [
+  {
+    name: 'Agustin Marteau',
+    role: 'Business development',
+    linkedin: 'https://www.linkedin.com/in/agustin-marteau-469275258/',
+    image: './agustin.jpg'
+  },
+  {
+    name: 'Joaquin Mármol',
+    role: 'Tech leader',
+    linkedin: 'https://www.linkedin.com/in/joaquin-marmol/',
+    image: './joaquin.webp'
+  },
+  {
+    name: 'Ezequiel Costales',
+    role: 'Product leader',
+    linkedin: 'https://www.linkedin.com/in/ezequiel-costales/',
+    image: './ezequiel.webp'
+  },
+  {
+    name: 'Serafin Quesada',
+    role: 'Front-end developer',
+    linkedin: 'https://www.linkedin.com/in/serafin-quesada/',
+    image: './serafin.webp'
+  },
+
+  {
+    name: 'David Juyneivch',
+    role: 'Product management',
+    linkedin: 'https://www.linkedin.com/in/david-juyneivch/',
+    image: './david.webp'
+  },
+  {
+    name: 'Maria Pia Zubillaga',
+    role: 'Design leader',
+    linkedin: 'https://www.linkedin.com/in/mariapiazubillaga/',
+    image: './pipi.webp'
+  },
+  {
+    name: 'Mateo Lonzayes',
+    role: 'Back-end developer',
+    linkedin: 'https://www.linkedin.com/in/mateo-lonzayes-perales-03b01b178/',
+    image: './mateo.webp'
+  }
+]
