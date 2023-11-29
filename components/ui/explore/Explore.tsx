e2bda05cedc244ccf2177b9159ddac58c6255ad3
JoaquinMarmol <joaquinmarmolnecochea@gmail.com>
2023-11-29 16:17:39 -0300

    marketplace sizes

--- components/ui/explore/Explore.tsx
+++ components/ui/explore/Explore.tsx
@@ -7,7 +7,7 @@ interface Props {
 	software: ISoftware[]
 }
 export const Explore: FC<Props> = ({ software }) => {
-	const pageSize = 6
+	const pageSize = 8
 	const totalProducts = software?.length
 	const totalPages = Math.ceil(totalProducts / pageSize)
 
@@ -29,12 +29,12 @@ export const Explore: FC<Props> = ({ software }) => {
 
 	return (
 		<div className="flex flex-col items-center justify-center">
-			<ul className="m-auto flex w-full flex-wrap items-center justify-center gap-4 md:px-4 md:py-4 md:pb-4 lg:py-8 2xl:gap-8">
+			<ul className="m-auto flex w-full flex-wrap items-center justify-center gap-4 md:px-4 md:py-4 md:pb-4 lg:p-0 lg:py-8 2xl:gap-8">
 				{displayedProducts.length > 0 ? (
 					displayedProducts?.map((product) => (
 						<li
 							key={product.id}
-							className="flex w-[45%] flex-col gap-12 lg:w-[30%] lg:gap-8 "
+							className="flex w-[45%] flex-col gap-12 lg:w-[30%] lg:gap-8 xl:w-[20%] "
 						>
 							<CardSoftware
 								id={product.id}
